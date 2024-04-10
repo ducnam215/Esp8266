d4ae62031f4da85fb76a8a0dea40312989ed48ce
Pham Duc Nam <PhamDucNam@gmail.com>
2024-04-10 22:46:02 +0700

    remove lib aht20

--- Mqtt_Esp8266_Client1/src/main.cpp
+++ Mqtt_Esp8266_Client1/src/main.cpp
@@ -1,40 +1,26 @@
 #include <Arduino.h>
-#include "DFRobot_AHT20.h"
 
-DFRobot_AHT20 aht20;
+const int ledPin = 0; // the number of the LED pin
+const int ledPin2 = 2;
+// variable for storing the pushbutton status
 
 void setup()
 {
-  Serial.begin(115200);
-  while (!Serial)
-  {
-    // Wait for USB serial port to connect. Needed for native USB port only
-  }
+  // initialize the pushbutton pin as an input
 
-  uint8_t status;
-  while ((status = aht20.begin()) != 0)
-  {
-    Serial.print("AHT20 sensor initialization failed. error status : ");
-    Serial.println(status);
-    delay(1000);
-  }
+  // initialize the LED pin as an output
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop()
 {
-  if (aht20.startMeasurementReady(/* crcEn = */ true))
-  {
-    Serial.print("temperature(-40~85 C): ");
-    // Get temp in Celsius (℃), range -40-80℃
-    Serial.print(aht20.getTemperature_C());
-    Serial.print(" C, ");
-    // Get temp in Fahrenheit (F)
-    Serial.print(aht20.getTemperature_F());
-    Serial.print(" F\t");
-    Serial.print("humidity(0~100): ");
-    // Get relative humidity (%RH), range 0-100℃
-    Serial.print(aht20.getHumidity_RH());
-    Serial.println(" %RH");
-    delay(1000);
-  }
+
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
+  delay(2000);
+
+  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin2, LOW);
+  delay(2000);
 }
